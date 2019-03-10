f7e1d3878bcc29b97f53e68a543060cd389b5372
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-03-10 11:17:36 -0700

    Init commit

---
+++ src/Game.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val name = "Madrigal"
+    var healthPoints = 89
+
+    if (healthPoints == 100){
+        println(name + " is in excellent condition!")
+    } else {
+        println(name + " is in awful condition!")
+    }
+}
