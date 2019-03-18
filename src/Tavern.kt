71f87464d95469bfade6cdd0662e3c51ea6631ee
Adrian Martinez <adrianjustinmartinez@gmail.com>
2019-03-18 14:01:01 -0700

    Add in coin purse

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,8 +1,33 @@
+import kotlin.math.roundToInt
+
 const val TAVERN_NAME = "Taernyl's Folly"
 
+var playerGold = 10
+var playerSilver = 10
+
 fun main(args: Array<String>) {
-//    placeOrder("shandy,Dragon's Breath,5.91")
-    placeOrder("elixir,Shirley's Temple,4.12")
+    placeOrder("shandy,Dragon's Breath,5.91")
+//    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+fun performPurchase(price: Double){
+    displayBalance()
+    val totalPurse = playerGold + (playerSilver / 100.0)
+    println("Total purse: $totalPurse")
+    println("Purchasing item for $price")
+
+    val remainingBalance = totalPurse - price
+    println("Remaining balance: ${"%.2f"}".format(remainingBalance))
+
+    val remainingGold = remainingBalance.toInt()
+    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+    playerGold = remainingGold
+    playerSilver = remainingSilver
+    displayBalance()
+}
+
+private fun displayBalance(){
+    println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
 }
 
 fun placeOrder(menuData: String) {
@@ -14,8 +39,10 @@ fun placeOrder(menuData: String) {
     val message = "Madrigal buys a $name ($type) for $price."
     println(message)
 
+    performPurchase(price.toDouble())
+
     val phrase = if (name == "Dragon's Breath") {
-        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+        "Madrigal exclaims ${toDragonSpeak("IT'S GOT WHAT ADVENTURERS CRAVE!")}"
     } else {
         "Madrigal says: Thanks for the $name"
     }
@@ -23,13 +50,13 @@ fun placeOrder(menuData: String) {
 }
 
 private fun toDragonSpeak(phrase:String) =
-        phrase.replace(Regex("[aeiou]")){
+        phrase.replace(Regex("[AEIOUaeiou]")){
             when (it.value){
-                "a" -> "4"
-                "e" -> "3"
-                "i" -> "1"
-                "o" -> "0"
-                "u" -> "|_|"
+                "a","A" -> "4"
+                "e","E" -> "3"
+                "i","I" -> "1"
+                "o","O" -> "0"
+                "u","U" -> "|_|"
                 else -> it.value
             }
         }
